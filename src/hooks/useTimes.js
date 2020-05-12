9b0205b82451d7bba78efa85e0de945940171738
= <=>
2020-05-11 20:20:23 -0500

    removed README.md

--- src/hooks/useTimes.js
+++ src/hooks/useTimes.js
@@ -4,7 +4,7 @@ const times = () => {
     let index = 0
     for(let hour=8; hour<=16; hour++) {
         for(let minute=0; minute<60; minute+=15) {
-            const newMinute = minute
+            let newMinute = minute
             if(newMinute < 10){newMinute = 0 + minute.toString()}
             times[index] = `${hour}:${newMinute}`
             index++
