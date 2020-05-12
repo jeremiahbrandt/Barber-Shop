90c8b2e7ee668ddb24193774b8c909deba1b4bd3
= <=>
2020-05-12 10:51:42 -0500

    stars SVG

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,51 @@
 import React from "react"
-import Img from 'gatsby-image'
 import Layout from "../components/layout"
 import Slideshow from "../components/Slideshow"
 import Map from "../components/Map"
 import { graphql } from "gatsby"
 import {useTestimonials} from '../hooks/useTestimonials'
-import {useStars} from '../hooks/useStars'
 import '../style/style.sass'
 import '../style/Testimonials.sass'
+import '../style/stars.sass'
 
-const Stars = (rating) => {
-  const stars = useStars()
-  const emptyStar = stars[0].node.childMarkdownRemark.frontmatter.image.childImageSharp.fixed
-  const halfStar = stars[1].node.childMarkdownRemark.frontmatter.image.childImageSharp.fixed
-  const fullStar = stars[2].node.childMarkdownRemark.frontmatter.image.childImageSharp.fixed
+const Stars = (props) => {
+  console.log('rating: ' + props.rating)
+
+  const Star = (rating, index) => {
+    const emptyStar = (
+      <svg xmlns="http://www.w3.org/2000/svg" className='star' strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <path d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" className='empty' />
+      </svg>
+    )
+    
+    const halfStar = (
+      <svg xmlns="http://www.w3.org/2000/svg" className='star' strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <path d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" className='full' />
+      </svg>
+    )
+
+    const fullStar = (
+      <svg xmlns="http://www.w3.org/2000/svg" className='star' strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <path d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" className='full' />
+      </svg>
+      
+    )
+      return fullStar
+    // if(rating-index >= 0){return fullStar}
+    // else if((rating-index) === -0.5){return halfStar}
+    // else{return emptyStar}
+  }
 
   return (
     <div className='rating'>
-      <Img fixed={fullStar} />
-      <Img fixed={fullStar} />
-      <Img fixed={fullStar} />
-      <Img fixed={fullStar} />
-      <Img fixed={fullStar} />
+      <Star type={props.rating, 1} />
+      <Star type={props.rating, 2} />
+      <Star type={props.rating, 3} />
+      <Star type={props.rating, 4} />
+      <Star type={props.rating, 5} />
     </div>
   )
 }
@@ -56,7 +80,7 @@ const IndexPage = ({data}) => (
     <Layout className='layout'>
         <Slideshow images={data.allFile.edges} />
         <TestimonialContainer />
-        <Map />
+        {/* <Map /> */}
     </Layout>
 )
 
